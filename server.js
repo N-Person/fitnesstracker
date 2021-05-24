38693f1fd16823a4d455fd4e79ddacca4cec9cf9
Brian Morgan <brianjm2007@yahoo.com>
2021-05-24 19:07:12 -0400

    created the server.sj

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const WorkoutController = require("./controllers/workoutController");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}, {useUnifiedTopology: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+   {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false} 
+);
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+console.log("Mongoose successfully connected");
+});
+connection.on("error", (err) => {
+    console.log("Mongoose connection error: ", err);
+});
+
+app.use(WorkoutController);
+require("./controllers/htmlController")(app);
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhoset:${PORT}');
+});
